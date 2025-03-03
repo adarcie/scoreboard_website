c564612100fec1cbdc6171ff8d34ac7e3e9dd54d
adarcie <38895262+adarcie@users.noreply.github.com>
2025-03-02 19:55:11 -0800

    moved around relative positions, decreased some border sizes

--- config.js
+++ config.js
@@ -5,6 +5,11 @@ positions = {
         stats: ['Aces','+Pass', '-Pass', '+Serve Rec','-Serve Rec'],
         names: ["Darby", "Emma"]
     },
+    setters: {
+        count: 2,
+        stats: ['Aces', 'Set:Hit', 'Set:Free-Ball', 'Set:Blocked'],
+        names: ["Laila", "Naila"]
+    },
     middles: {
         count: 2,
         // Fixed spelling error: "Attack Errors"
@@ -16,16 +21,13 @@ positions = {
         stats: ['Aces', 'Kills', 'Blocks', 'Attack Errors', '+Serve Rec','-Serve Rec'],
         names: ["Julie", "Olivia"]
     },
+    
     outsides: {
         count: 5,
         stats: ['Aces', 'Kills', 'Blocks', 'Attack Errors', '+Serve Rec','-Serve Rec'],
         names: ["Gabi", "Hannah", "Lilianna", "Riley", "Shirley"]
-    },
-    setters: {
-        count: 2,
-        stats: ['Aces', 'Set:Hit', 'Set:Free-Ball', 'Set:Blocked'],
-        names: ["Laila", "Naila"]
     }
+    
 };
 
 // Override password (plain text)
