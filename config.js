00f1d8edbfa3e00e2a708576909a1af54e8ca011
adarcie <38895262+adarcie@users.noreply.github.com>
2025-03-02 19:37:32 -0800

    added individual players

--- config.js
+++ config.js
@@ -2,28 +2,29 @@
 positions = {
     liberos: {
         count: 2,
-        stats: ['CustomStat1', 'CustomStat2'],
-        names: ["NewL1", "NewL2"]
+        stats: ['Aces','+Pass', '-Pass', '+Serve Rec','-Serve Rec'],
+        names: ["Darby", "Emma"]
     },
     middles: {
         count: 2,
-        stats: ['CustomStat3', 'CustomStat4'],
-        names: ["NewM1", "NewM2"]
+        // Fixed spelling error: "Attack Errors"
+        stats: ['Aces', 'Kills', 'Blocks', 'Attack Errors'],
+        names: ["Alicia", "Melia"]
     },
     rightSides: {
         count: 2,
-        stats: ['CustomStat5', 'CustomStat6'],
-        names: ["NewRS1", "NewRS2"]
+        stats: ['Aces', 'Kills', 'Blocks', 'Attack Errors', '+Serve Rec','-Serve Rec'],
+        names: ["Julie", "Olivia"]
     },
     outsides: {
         count: 5,
-        stats: ['CustomStat7', 'CustomStat8'],
-        names: ["NewO1", "NewO2", "NewO3", "NewO4", "NewO5"]
+        stats: ['Aces', 'Kills', 'Blocks', 'Attack Errors', '+Serve Rec','-Serve Rec'],
+        names: ["Gabi", "Hannah", "Lilianna", "Riley", "Shirley"]
     },
     setters: {
         count: 2,
-        stats: ['CustomStat9', 'CustomStat10'],
-        names: ["NewS1", "NewS2"]
+        stats: ['Aces', 'Set:Hit', 'Set:Free-Ball', 'Set:Blocked'],
+        names: ["Laila", "Naila"]
     }
 };
 
